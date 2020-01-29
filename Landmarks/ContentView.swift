33f68b21687456501291727ca3b7a06c6d8d024c
Will <williamsouef@gmail.com>
2020-01-29 06:43:52 -0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by William Souef on 29/01/2020.
+//  Copyright © 2020 William Souef. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
